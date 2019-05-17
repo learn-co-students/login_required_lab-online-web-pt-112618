a8ea0f500a6e8fa0f775783dfe87dc9066e8dc0a
Michael Schiumo <mschiumo18@gmail.com>
2019-05-17 13:56:48 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    return @current_user.name
+
   end
 end
